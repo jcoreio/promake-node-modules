2e878fc4177ac285f04902e9717faaad4d70d795
Andy Edwards <jedwards@fastmail.com>
2018-06-09 12:54:15 -0500

    feat: initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,67 @@
-/* @flow */
+// @flow
 
-/* eslint-disable no-console, no-undef */
-console.log('Hello world!')
+import path from 'path'
+import Promake from 'promake'
+import type Rule from 'promake/lib/Rule'
+import fs from 'fs'
+import promisify from 'es6-promisify'
+import _glob from 'glob'
+import touch from 'touch'
+
+const {VERBOSITY} = Promake
+
+const glob = promisify(_glob)
+const stat = promisify(fs.stat)
+
+async function lastModified(file: string): Promise<?number> {
+  try {
+    const {mtimeMs, mtime} = await stat(file)
+    return mtimeMs != null ? mtimeMs : mtime.getTime()
+  } catch (err) {
+    if (err.code === 'ENOENT') return null
+    throw err
+  }
+}
+
+function nodeModulesRecipe(installNodeModules?: (rule: Rule) => ?Promise<any>): (rule: Rule) => ?Promise<any> {
+  return async function installNodeModulesIfNecessary(rule: Rule): Promise<any> {
+    const log = rule ? rule.promake.log : (...args: Array<any>) => {}
+
+    const {targets, prerequisites} = rule
+    if (targets.length > 1) throw new Error('there must be only one target (the node_modules directory)')
+    const target = targets[0]
+    const targetDir = (target: any).file
+    if (typeof targetDir !== 'string') throw new Error('target must have a `file` property that is the node_modules directory')
+
+    const packages = [...await glob(path.join(targetDir, '*')), ...await glob(path.join(targetDir, '@*', '*'))]
+    if (packages.length) {
+      const packageMtimes = await Promise.all(packages.map(lastModified))
+      const prerequisiteMtimes = await Promise.all(prerequisites.map(
+        async prerequisite => {
+          const mtime = await prerequisite.lastModified()
+          return mtime != null ? mtime : -Infinity
+        }
+      ))
+      const maxPackageMtime = Math.max(...packageMtimes)
+      const maxPrerequisiteMtime = Math.max(...prerequisiteMtimes)
+
+      if (maxPackageMtime > maxPrerequisiteMtime) {
+        log(VERBOSITY.DEFAULT, 'Nothing to be done for', target)
+        return
+      }
+    }
+
+    if (installNodeModules) {
+      await installNodeModules(rule)
+    } else {
+      const {promake: {spawn}} = rule
+      const hasYarn = prerequisites.find(prerequisite =>
+        path.resolve(prerequisite.file) === path.resolve(targetDir, '..', 'yarn.lock')
+      )
+      if (hasYarn) await spawn('yarn', ['--ignore-scripts'])
+      else await spawn('npm', ['install', '--ignore-scripts'])
+    }
+    await touch(targetDir)
+  }
+}
+exports.nodeModulesRecipe = nodeModulesRecipe
--- test/index.js
+++ test/index.js
@@ -1,9 +1,39 @@
 // @flow
 
-import '../src/index'
+import {describe, it} from 'mocha'
+import {exec} from 'promisify-child-process'
+import {expect} from 'chai'
+import fs from 'fs-extra'
+import touch from 'touch'
+import path from 'path'
+import promisify from 'es6-promisify'
+import delay from 'delay'
 
-describe('test setup', () => {
-  it('works', () => {
+async function getMTime(file: string): Promise<number> {
+  return (await promisify(fs.stat)(file)).mtime.getTime()
+}
 
+const cwd = path.resolve(__dirname, 'integration')
+const packageJsonFile = path.resolve(cwd, 'node_modules')
+const nodeModulesDir = path.resolve(cwd, 'node_modules')
+
+describe('nodeModulesRecipe', function () {
+  this.timeout(15 * 60000)
+
+  it('installs when necessary', async function (): Promise<void> {
+    await fs.remove(nodeModulesDir)
+    const {stderr} = await exec(`babel-node promake node_modules`, {cwd})
+    expect(stderr).not.to.match(/^\[promake\] Nothing to be done for node_modules$/m)
+    expect(await getMTime(nodeModulesDir)).not.to.be.below(await getMTime(packageJsonFile))
+  })
+  it("doesn't install when unnecessary", async function (): Promise<void> {
+    await exec(`npm i --ignore-scripts`, {cwd})
+    await delay(1000)
+    await touch(packageJsonFile, {mtime: true})
+    // await delay(100)
+    // await touch(path.resolve(nodeModulesDir, 'lodash.omit'), {mtime: true})
+    const {stdout, stderr} = await exec(`babel-node promake node_modules`, {cwd})
+    console.log(stdout)
+    expect(stderr).to.match(/^\[promake\] Nothing to be done for node_modules$/m)
   })
 })
